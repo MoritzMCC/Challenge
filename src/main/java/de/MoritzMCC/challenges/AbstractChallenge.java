78c45f317a628e86ac7f82f7009a6c09e16e205a
MoritzMCC <88977881+MoritzMCC@users.noreply.github.com>
2025-01-06 16:38:49 +0100

    add testchallenge

--- src/main/java/de/MoritzMCC/challenges/AbstractChallenge.java
+++ src/main/java/de/MoritzMCC/challenges/AbstractChallenge.java
@@ -3,6 +3,10 @@ package de.MoritzMCC.challenges;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.ItemStack;
@@ -51,5 +55,52 @@ public class AbstractChallenge implements Listener {
             onSneak(event);
         }
     }
+
+    public void onPlayerDamage(EntityDamageEvent event){}
+    public void onEntityDamage(EntityDamageEvent event){}
+
+    @EventHandler
+    public void Damage(EntityDamageEvent event){
+        if (event.getEntity() instanceof Player){
+            onPlayerDamage(event);
+            return;
+        }
+        onEntityDamage(event);
+    }
+
+    public void onAdvancement(PlayerAdvancementDoneEvent event){}
+
+    @EventHandler
+    public void Advancment(PlayerAdvancementDoneEvent event){
+        onAdvancement(event);
+    }
+
+    public void onPlayerGetHitByEntity(EntityDamageByEntityEvent event){}
+
+    public void onPlayerHitEntity(EntityDamageByEntityEvent event){}
+
+    public void onPlayerHitPlayer(EntityDamageByEntityEvent event){}
+
+    @EventHandler
+    public void PlayerHitEntity(EntityDamageByEntityEvent event){
+
+        if(event.getEntity() instanceof Player && event.getDamager() instanceof Player){
+            onPlayerHitPlayer(event);
+            return;
+        }
+        if (event.getEntity() instanceof Player){
+            onPlayerGetHitByEntity(event);
+            return;
+        }
+        if(event.getDamager() instanceof Player){
+            onPlayerHitEntity(event);
+        }
+
+    }
+
+
+  
+
+
 
 }
--- src/main/java/de/MoritzMCC/challenges/someChallenges/NoSneakChallenge.java
+++ src/main/java/de/MoritzMCC/challenges/someChallenges/NoSneakChallenge.java
@@ -1,11 +1,16 @@
 package de.MoritzMCC.challenges.someChallenges;
 
 import de.MoritzMCC.challenges.AbstractChallenge;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.inventory.Inventory;
 import org.checkerframework.checker.units.qual.A;
+
+import java.awt.*;
+import java.text.MessageFormat;
 
 public class NoSneakChallenge extends AbstractChallenge {
 
@@ -16,6 +21,9 @@ public class NoSneakChallenge extends AbstractChallenge {
 
     @Override
     public void onSneak(PlayerToggleSneakEvent event ) {
-        event.setCancelled(true);
+        event.getPlayer().setHealth(0);
+        Bukkit.broadcastMessage("You failed this challenge");
+
     }
+
 }
